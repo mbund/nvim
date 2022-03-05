b85d9155b25833f98fe1e213d21902b61ddf98d3
mbund <bundschuhmark@gmail.com>
2022-03-05 18:09:11 -0500

    update lock, move language packages out, update reademe

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # nvim
 Personal neovim setup as a fully featured IDE
+
+## Installation and usage
+Add this repository as an input to your flake, and use its
+default package. It would look something like this:
+```nix
+{
+  inputs.nvim.url = "github:mbund/nvim";
+  # ...
+  outputs = { ... }@inputs: {
+    # now add this inside your home.packages = [] or environment.systemPackages = []
+    inputs.nvim.defaultPackage.${pkgs.system}
+  };
+}
+```
+
+Or you can run it directly without installation with
+```
+nix run github:mbund/nvim
+```
+
+## Supported languages
+These languages have integration into this neovim configuration,
+if you have the appropriate packages installed. The package names
+are what they are in `nixpkgs`, and I would recommend that you
+add them as dependencies in your nix development shell for each
+individual project.
+
+### lua
+sumneko-lua-language-server
+stylua
+lua53Packages.luacheck
+
+### nix
+rnix-lsp
+
+### haskell
+haskell-language-server
+haskellPackages.fourmolu
+
+### rust
+rust-analyzer
+rustfmt
+
+### python
+python39Packages.autopep8
+python39Packages.flake8
+nodePackages.pyright
+
+### javascript/typescript
+nodePackages.eslint_d
+
+### c/c++
+clang
+clang-tools
+cppcheck
+
--- flake.nix
+++ flake.nix
@@ -65,113 +65,99 @@
 
       in
       rec {
-        packages.nvim = neovimBuilder
-          {
-            package = pkgs.neovim-nightly;
-            customLua = builtins.readFile ./init.lua;
-            dir = ./.;
-            extraPackages = with pkgs; [
-              neovim-remote
-              (tree-sitter.withPlugins (p: builtins.attrValues p)) # activiate all tree-sitter languages
-              xclip
-              ripgrep # required for telescope
-              fd # required for telescope
+        packages.nvim = neovimBuilder {
+          package = pkgs.neovim-nightly;
+          customLua = builtins.readFile ./init.lua;
+          dir = ./.;
+          extraPackages = with pkgs; [
+            neovim-remote
+            (tree-sitter.withPlugins (p: builtins.attrValues p)) # activiate all tree-sitter languages
+            xclip
+            ripgrep # required for telescope
+            fd # required for telescope
+          ];
+          start =
+            let
+              externalPlugin = pkgs.vimUtils.buildVimPluginFrom2Nix;
+            in
+            with pkgs.vimPlugins;
+            [
+              # dependencies
+              plenary-nvim
+              popup-nvim
+              nvim-web-devicons
 
-              sumneko-lua-language-server
-              rnix-lsp
-              haskell-language-server
-              rust-analyzer
-              nodePackages.bash-language-server
-              nodePackages.typescript-language-server
-              nodePackages.vim-language-server
-              python39Packages.autopep8
-              python39Packages.flake8
-              nodePackages.pyright
-              rustfmt
-              nodePackages.eslint_d
-              stylua
-              clang
-              clang-tools
-              cppcheck
-              lua53Packages.luacheck
-              haskellPackages.fourmolu
+              # UI
+              onedark-vim
+              tokyonight-nvim
+              (externalPlugin { pname = "startup-nvim"; version = "master"; src = inputs.startup-nvim; })
+              bufferline-nvim
+              nvim-tree-lua
+              telescope-nvim
+              telescope-fzf-native-nvim
+              telescope-file-browser-nvim
+              harpoon
+              (externalPlugin { pname = "nvim-code-action-menu"; version = "master"; src = inputs.nvim-code-action-menu; })
+              (externalPlugin { pname = "nvim-lightbulb"; version = "master"; src = inputs.nvim-lightbulb; })
+              lsp_signature-nvim
+              vim-hexokinase
+              nvim-cursorline
+              nvim-treesitter
+              nvim-treesitter-context
+              gitsigns-nvim
+              lualine-nvim
+              trouble-nvim
+
+              # motions, remaps, text-editing improvements
+              vim-sneak
+              vim-commentary
+              vim-indent-object
+              vim-textobj-user
+              vim-sort-motion
+              vim-exchange
+              vim-unimpaired
+              vim-surround
+              vim-repeat
+              nvim-autopairs
+              vim-sleuth
+
+              # completion
+              nvim-cmp
+              cmp-buffer
+              luasnip
+              cmp_luasnip
+              lspkind-nvim
+              cmp-path
+              cmp-treesitter
+
+              # lsp
+              nvim-lspconfig
+              nvim-lsputils
+              null-ls-nvim
+              cmp-nvim-lsp
+
+              # rust
+              crates-nvim
+              rust-tools-nvim
+
+              # web development
+              nvim-ts-autotag
+
             ];
-            start =
-              let
-                externalPlugin = pkgs.vimUtils.buildVimPluginFrom2Nix;
-              in
-              with pkgs.vimPlugins;
-              [
-                # dependencies
-                plenary-nvim
-                popup-nvim
-                nvim-web-devicons
-
-                # UI
-                onedark-vim
-                tokyonight-nvim
-                (externalPlugin { pname = "startup-nvim"; version = "master"; src = inputs.startup-nvim; })
-                bufferline-nvim
-                nvim-tree-lua
-                telescope-nvim
-                telescope-fzf-native-nvim
-                telescope-file-browser-nvim
-                harpoon
-                (externalPlugin { pname = "nvim-code-action-menu"; version = "master"; src = inputs.nvim-code-action-menu; })
-                (externalPlugin { pname = "nvim-lightbulb"; version = "master"; src = inputs.nvim-lightbulb; })
-                lsp_signature-nvim
-                vim-hexokinase
-                nvim-cursorline
-                nvim-treesitter
-                nvim-treesitter-context
-                gitsigns-nvim
-                lualine-nvim
-                trouble-nvim
-
-                # motions, remaps, text-editing improvements
-                vim-sneak
-                vim-commentary
-                vim-indent-object
-                vim-textobj-user
-                vim-sort-motion
-                vim-exchange
-                vim-unimpaired
-                vim-surround
-                vim-repeat
-                nvim-autopairs
-                vim-sleuth
-
-                # completion
-                nvim-cmp
-                cmp-buffer
-                luasnip
-                cmp_luasnip
-                lspkind-nvim
-                cmp-path
-                cmp-treesitter
-
-                # lsp
-                nvim-lspconfig
-                nvim-lsputils
-                null-ls-nvim
-                cmp-nvim-lsp
-
-                # rust
-                crates-nvim
-                rust-tools-nvim
-
-                # web development
-                nvim-ts-autotag
-
-              ];
-          };
+        };
 
         apps.nvim = { type = "app"; program = "${packages.nvim}/bin/nvim"; };
 
         defaultPackage = packages.nvim;
         defaultApp = apps.nvim;
 
-        devShell = pkgs.mkShell { buildInputs = [ packages.nvim ]; };
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            sumneko-lua-language-server
+            stylua
+            rnix-lsp
+          ];
+        };
 
         overlay = (self: super: {
           inherit neovimBuilder;
@@ -180,39 +166,3 @@
       }
     );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
